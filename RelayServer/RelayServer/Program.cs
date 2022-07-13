87892f493126281cd31df929ceacdd551beb91a9
csd3410 <csd3410@csd.uoc.gr>
2022-07-13 12:06:12 +0300

    Server First try

--- RelayServer/RelayServer/Program.cs
+++ RelayServer/RelayServer/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RiptideNetworking;
 
 namespace RelayServer
 {
@@ -10,6 +11,11 @@ namespace RelayServer
     {
         static void Main(string[] args)
         {
+            Console.Title = "Relay Server";
+            Server _Server = new Server();
+            _Server.Start(7777, 10);
+            Console.ReadKey();
+            _Server.Stop();
         }
     }
 }
